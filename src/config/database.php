d63f40f4948a88780e2b6ae4fa3295b43c1d671b
Luthfan Putra Prakarsa <luthfanputra.04@gmail.com>
2026-04-20 08:50:02 +0700

    refactor: add sqlsrv multi connection

--- src/config/database.php
+++ src/config/database.php
@@ -100,19 +100,34 @@ return [
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
+        // db_app
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'db_app'),
+            'username' => env('DB_USERNAME', 'sa'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+        ],
+
+        // db master (MINA_IOT)
+        'sqlsrv_master' => [
+            'driver' => 'sqlsrv',
+            'url' => env('DB_MASTER_URL'),
+            'host' => env('DB_MASTER_HOST', '127.0.0.1'),
+            'port' => env('DB_MASTER_PORT', '1433'),
+            'database' => env('DB_MASTER_DATABASE', 'MINA_IOT_PKL'),
+            'username' => env('DB_MASTER_USERNAME', 'user_readonly'),
+            'password' => env('DB_MASTER_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
         ],
 
     ],
